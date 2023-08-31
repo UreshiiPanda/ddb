0373c809e7d217d9e48c3a1028cac19ba990c647
UreshiiPanda <jojohoughton22@gmail.com>
2023-08-31 17:29:25 -0400

    removed extra files

--- mysql_initializer.py
+++
@@ -1,9 +0,0 @@
-"""
-Separate the mysql connection so that it can be imported in both ../app.py
-and the .py files in ../routes directory.
-This prevents circular import error if it were to be initialized in app.py
-"""
-
-from flask_mysqldb import MySQL
-
-mysql = MySQL()          # Now mysql can be imported by app.py and any other .py files
